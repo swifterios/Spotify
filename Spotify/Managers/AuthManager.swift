b616038ec289cfda6e715c4c6953a997bb8562ca
Vladislav <lordroga2@gmail.com>
2021-07-29 18:44:11 +0300

    AuthManager adds + cache

--- Spotify/Managers/AuthManager.swift
+++ Spotify/Managers/AuthManager.swift
@@ -13,6 +13,7 @@ final class AuthManager {
     struct Constants {
         static let cliendID = "3a9a0420d34a40d6bcb2b0ea29be9e90"
         static let cliendSecret = "c713e513c5ad46cca5016e83010bc45e"
+        static let tokenAPIURL = "https://accounts.spotify.com/api/token"
     }
     
     public var signInURL: URL? {
@@ -27,26 +28,76 @@ final class AuthManager {
     private init() {}
     
     var isSignedIn: Bool {
-        return false
+        return accessToken != nil
     }
     
     private var accessToken: String? {
-        return nil
+        return UserDefaults.standard.string(forKey: "access_token")
     }
     
     private var refreshToken: String? {
-        return nil
+        return UserDefaults.standard.string(forKey: "refresh_token")
     }
     
-    private var expirationDate: Date? {
-        return nil
+    private var tokenExpirationDate: Date? {
+        return UserDefaults.standard.object(forKey: "expirationDate") as? Date
     }
     
     private var shouldRefreshToken: Bool {
-        return false
+        guard let expirationDate = tokenExpirationDate else {
+            return false
+        }
+        
+        let currentDate = Date()
+        let fiveMinutes: TimeInterval = 300
+        return currentDate.addingTimeInterval(fiveMinutes) >= expirationDate
     }
     
     public func exchangeCodeForToken(code: String, completion: @escaping (Bool) -> Void) {
+        guard let url = URL(string: Constants.tokenAPIURL) else {
+            return
+        }
+        
+        var components = URLComponents()
+        components.queryItems = [
+            URLQueryItem(name: "grant_type", value: "authorization_code"),
+            URLQueryItem(name: "code", value: code),
+            URLQueryItem(name: "redirect_uri", value: "https://github.com/swifterios"),
+        ]
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = components.query?.data(using: .utf8)
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        
+        let basicToken = Constants.cliendID + ":" + Constants.cliendSecret
+        let data = basicToken.data(using: .utf8)
+        guard let base64String = data?.base64EncodedString() else {
+            print("Failure to get base64")
+            completion(false)
+            return
+        }
+        
+        request.setValue("Basic \(base64String)", forHTTPHeaderField: "Authorization")
+        
+        let task = URLSession.shared.dataTask(with: request) { [weak self] data, _, error in
+            guard let data = data, error == nil else {
+                completion(false)
+                return
+            }
+            
+            do {
+                let result = try JSONDecoder().decode(AuthResponce.self, from: data)
+                self?.cacheToken(result: result)
+                print(result)
+                completion(true)
+            }
+            catch {
+                print(error.localizedDescription)
+                completion(false)
+            }
+        }
+        task.resume()
         
     }
     
@@ -54,7 +105,10 @@ final class AuthManager {
         
     }
     
-    private func cacheToken() {
+    private func cacheToken(result: AuthResponce) {
+        UserDefaults.standard.setValue(result.access_token, forKey: "access_token")
+        UserDefaults.standard.setValue(result.refresh_token, forKey: "refresh_token")
+        UserDefaults.standard.setValue(Date().addingTimeInterval(TimeInterval(result.expires_in)),forKey: "expirationDate")
         
     }
 }
---
+++ Spotify/Models/AuthResponse.swift
@@ -0,0 +1,16 @@
+//
+//  AuthResponse.swift
+//  Spotify
+//
+//  Created by Владислав on 29.07.2021.
+//
+
+import Foundation
+
+struct AuthResponce: Codable {
+    let access_token: String
+    let expires_in: Int
+    let refresh_token: String
+    let scope: String
+    let token_type: String
+}
