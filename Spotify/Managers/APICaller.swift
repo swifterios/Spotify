86b04e797063bbe262446c5816d5a6327c8965e6
Vladislav <lordroga2@gmail.com>
2021-07-31 22:05:26 +0300

    create request API Calls

--- Spotify/Managers/APICaller.swift
+++ Spotify/Managers/APICaller.swift
@@ -13,6 +13,28 @@ final class APICaller {
     private init() {}
     
     public func getCurrentUserProfile(completion: @escaping (Result<UserProfile, Error>) -> Void) {
-        
+        AuthManager.shared.withValidToken { token in
+            
+        }
+    }
+    
+    enum HTTPMethod: String {
+        case GET
+        case POST
+    }
+    
+    private func createRequest(with url: URL?,
+                               type: HTTPMethod,
+                               completion: @escaping (URLRequest) -> Void){
+        AuthManager.shared.withValidToken { token in
+            guard let apiUrl = url else {
+                return
+            }
+            var request = URLRequest(url: apiUrl)
+            request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+            request.httpMethod = type.rawValue
+            request.timeoutInterval = 30
+            completion(request)
+        }
     }
 }
