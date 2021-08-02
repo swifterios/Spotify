e264defe03759f37be08f36b802788b145095f08
Vladislav <lordroga2@gmail.com>
2021-08-02 12:21:31 +0300

    get usersProfile data

--- Spotify/Managers/APICaller.swift
+++ Spotify/Managers/APICaller.swift
@@ -17,6 +17,19 @@ final class APICaller {
         static let baseAPIURL = "https://api.spotify.com/v1"
     }
     
+    //MARK: - Enums
+    
+    enum APIError: Error {
+        case faileedToGetData
+    }
+    
+    enum HTTPMethod: String {
+        case GET
+        case POST
+    }
+    
+    //MARK: - API functions
+    
     public func getCurrentUserProfile(completion: @escaping (Result<UserProfile, Error>) -> Void) {
         createRequest(with: URL(string: Constants.baseAPIURL + "/me"),
                       type: .GET) { baseRequest in
@@ -28,7 +41,7 @@ final class APICaller {
                 }
                 
                 do {
-                    let result = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+                    let result = try JSONDecoder().decode(UserProfile.self, from: data)
                     print(result)
                 }
                 catch {
@@ -39,29 +52,23 @@ final class APICaller {
         }
     }
     
-    private func createRequest(with url: URL?,
-                               type: HTTPMethod,
-                               completion: @escaping (URLRequest) -> Void){
-        AuthManager.shared.withValidToken { token in
-            guard let apiUrl = url else {
-                return
+    private func createRequest(
+            with url: URL?,
+            type: HTTPMethod,
+            completion: @escaping (URLRequest) -> Void
+        ) {
+            AuthManager.shared.withValidToken { token in
+                guard let apiURL = url else {
+                    return
+                }
+                var request = URLRequest(url: apiURL)
+                request.setValue("Bearer \(token)",
+                                 forHTTPHeaderField: "Authorization")
+                request.httpMethod = type.rawValue
+                request.timeoutInterval = 30
+                completion(request)
             }
-            var request = URLRequest(url: apiUrl)
-            request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
-            request.httpMethod = type.rawValue
-            request.timeoutInterval = 30
-            completion(request)
         }
-    }
     
-    //MARK: - Enums
-    
-    enum APIError: Error {
-        case faileedToGetData
-    }
-    
-    enum HTTPMethod: String {
-        case GET
-        case POST
-    }
+
 }
